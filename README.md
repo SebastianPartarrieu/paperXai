36fd767110abb788a477d5586b441888e284e65b
Sebastian Partarrieu <spartarrieu@joko.com>
2023-07-30 20:38:12 +0200

    first report HTML version

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# paperXai
+# paperXai [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) ![Python](https://img.shields.io/badge/python-3.9-blue.svg) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
 
 Current ambition: Your arXiv daily digest - brought to you by your favorite LLM provider.
 
-This is a **very early-stage project** which aims to sift through all the latest papers in AI posted on arXiv and filter according to your interests before giving you a short summary. The main pain point we're trying to solve is the sheer mass and noise of current information around AI. Even when we keep it to the latest papers, it's very hard to select the most interesting ones.
+This is a **very early-stage project** which aims to sift through all the latest papers in AI posted on arXiv and filter according to your interests before giving you a short summary. The main pain point we're trying to solve is the sheer mass and noise of current information around AI.
 
-Future ambitions: aggregate across multiple news sources and create a fully automatic, personalized, newsletter that we can each tweak according to what we want to read. There are a few great newsletters out there and I'm not saying these will be out of business just yet; however, why can't I read something completely tailored to my interests that was intelligently put together and contains actionable information?
+Future ambitions: aggregate across multiple news sources and create a fully automatic, personalized, newsletter that we can each tweak according to what we want to read. There are a few great newsletters out there and I'm not saying these will be out of business just yet; however, why can't I read something completely tailored to my interests?
 
 # Installation
 
@@ -13,19 +13,28 @@ Future ambitions: aggregate across multiple news sources and create a fully auto
 - `pip install -e .`
 - go to `src/paperxai`, create a `credentials.py` file and enter fill in `OPENAI_API_KEY = "your-key-here"`
 
+Once you've finished the installation procedure, a good place to start may the `notebooks/example_workflow.ipynb` notebook which gives a good overview of the different parts of the package.
+
 # How to use this
+
+Option #1 -> use the streamlit webapp (`cd display`, `streamlit run webapp.py`)
+Option #2 -> just rerun the `example_workflow.ipynbn` notebook every so often. In it, you'll be able to load the HTML and have a readable report.
+Option #3 (Incoming) -> through the newsletter service which will be setup
 
 # Testing
 
 # Development
 
-Any contributions are welcome. Starting out as a solo project, I took the **very bad** habit of using only the master branch before using a cleaner feature branch based development process.
+Any contributions are welcome. Starting out as a solo project, I took the **very bad** habit of using only the master branch before using a cleaner feature branch based development process. There are also some arbitrary choices that have been made (such as using some minimalist modules instead of using libraries like langchain).
 
 (checklist) before pushing changes or opening a PR:
 
 - `pip list --format=freeze > requirements.txt`
 - remove `paperxai` from the requirements and add `pip install -e .`
-- make sure all code is clean, documented and you've tested any core changes
+
+# Disclaimer
+
+This does not substitute discovering papers/information through the multitude of other ways. It's useful if you have a few predefined topics and want to sift through the volume of incoming information.
 
 # Thanks
 
--- src/paperxai/report/retriever.py
+++ src/paperxai/report/retriever.py
@@ -22,6 +22,8 @@ class ReportRetriever:
         self.papers_embedding = papers_embedding
         self.df_papers = df_papers
         self.config = load_config(path_to_config_file)
+        self.report = {}
+        self.report_papers = pd.DataFrame()
 
 
     def print_report(self) -> None:
@@ -30,17 +32,39 @@ class ReportRetriever:
         """
         report_string = ""
         for section_title, section_info in self.report.items():
-            report_string += "Section: " + section_title + "\n"
+            report_string += "Section: " + section_title + "\n\n"
             if isinstance(section_info["questions"], list):
                 for i in range(len(section_info["questions"])):
                     report_string += f"Question: {section_info['questions'][i]}" + "\n"
                     report_string+= f"LLM response: {section_info['chat_responses'][i]}" + "\n"
                 report_string += "\n"
-            else:
-                report_string += f"Question: {section_info['questions']}" + "\n"
-                report_string+= f"LLM response: {section_info['chat_responses']}" + "\n"
         print(report_string)
-        self.report_string = report_string
+        
+
+    def format_report(self) -> None:
+        report_html_string  = ""
+        for section_title, section_info in self.report.items():
+            report_html_string += "<h2> Section: " + section_title + "</h2>"
+            if isinstance(section_info["questions"], list):
+                for i in range(len(section_info["questions"])):
+                    report_html_string += "<h3> Question: " + section_info['questions'][i] + "</h3>"
+                    report_html_string += "<p> LLM response: " + section_info['chat_responses'][i] + "</p>"
+                    # add papers
+                    report_html_string += "<h4> Papers </h4>"
+                    report_html_string += self.format_paper_df_to_html_citation(section_info['papers'][i])
+        return report_html_string
+
+
+    def format_paper_df_to_html_citation(self, df_papers:pd.DataFrame) -> pd.DataFrame:
+        """
+        Format the dataframe of papers to html citations
+        """
+        citation_list = []
+        for _, row in df_papers.iterrows():
+            citation_list.append(
+                f"<li> {row['Title']}. {row['Authors'].split(',')[0].split(' ')[-1]} et al. {row['Published Date'].split(' ')[0]}</li>"
+            )
+        return "<ul>" + "".join(citation_list) + "</ul>"
 
     def create_report(self) -> dict:
         """
@@ -51,31 +75,35 @@ class ReportRetriever:
         sections = self.config["sections"]
         for section_number, section_info in sections.items():
             print("Getting responses for section: " + section_info["title"])
-            chat_responses = [
-                self.get_chat_response_to_question(section_info["questions"][i]) for i in range(len(section_info["questions"]))
+            responses = [
+                self.get_chat_response_and_papers_to_question(section_info["questions"][i]) for i in range(len(section_info["questions"]))
             ]
+            chat_responses = [response[0] for response in responses]
+            top_k_papers = [response[1] for response in responses]
             report[section_info["title"]] = {
                 "questions": section_info["questions"],
                 "chat_responses": chat_responses,
+                "papers": top_k_papers
             }
         self.report = report
         return report
 
 
-    def get_chat_response_to_question(self, question: str) -> str:
+    def get_chat_response_and_papers_to_question(self, question: str) -> tuple[str, pd.DataFrame]:
         """
         Embed question, retrieved top k papers and feed them as context
         to the language model to get a summary
         """
-        embedded_question = self.language_model.get_embeddings(question)
+        print("Answering question: " + question)
         top_k_papers = self.retrieve_top_k_papers(question, top_k = 3)
+        self.report_papers = pd.concat([self.report_papers, top_k_papers])
         prompt = self.prompter.create_prompt_for_report(
             question, top_k_papers
         )
         chat_response = self.language_model.get_chat_response(
             prompt
         )
-        return chat_response
+        return chat_response, top_k_papers
 
     def retrieve_top_k_papers(self, query: str, top_k: int = 10) -> list[int]:
         """
